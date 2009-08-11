88f23c930f5f49d018c37a92e3f180b1e48861ee
ejucovy <ejucovy@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-08-11 16:59:41 +0000

    rote renames
    git-svn-id: http://svn.ccnmtl.columbia.edu/elggmdp/plugins/new_profile_redirector/trunk@15919 1f418930-7ff8-0310-b8ad-c653122473bc

--- languages/en.php
+++ languages/en.php
@@ -1,14 +1,14 @@
 <?php
 	$english = array(
 		// Main title
-		'first_login_redirector' => "Login Redirector",
+		'new_profile_redirector' => "New Profile Redirector",
 		
 		// Admin configuration
-		'first_login_redirector:admin:config' => "Select the page users will go to after they are logged in:",
-		'first_login_redirector:admin:option:custom_redirect' => "Use custom redirect",
+		'new_profile_redirector:admin:config' => "Select the page users will go to after they first save a profile:",
+		'new_profile_redirector:admin:option:custom_redirect' => "Use custom redirect",
 	
-		'first_login_redirector:admin:custom_redirect' => "Enter a custom redirect here",
-		'first_login_redirector:admin:custom_redirect_info' => "To redirect a user to a site or page of your liking, enter the custom url here.<br/>Enter the complete url (including http or https) or use one of the following shortcuts (including brackets):",
+		'new_profile_redirector:admin:custom_redirect' => "Enter a custom redirect here",
+		'new_profile_redirector:admin:custom_redirect_info' => "To redirect a user to a site or page of your liking, enter the custom url here.<br/>Enter the complete url (including http or https) or use one of the following shortcuts (including brackets):",
 		
 	);
 	
