16d6c6edcba54ac8dabb56ab5c9151b8cb70354e
ejucovy <ejucovy@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-08-11 16:41:26 +0000

    get rid of stuff we dont need
    git-svn-id: http://svn.ccnmtl.columbia.edu/elggmdp/trunk/elgg1.5/mod_thirdparty/login_redirector@15912 1f418930-7ff8-0310-b8ad-c653122473bc

--- views/default/settings/login_redirector/edit.php
+++ views/default/settings/login_redirector/edit.php
@@ -1,12 +1,4 @@
 <?php
-	/**
-	* Login Redirector
-	* 
-	* @package login_redirector
-	* @author ColdTrick IT Solutions
-	* @copyright Coldtrick IT Solutions 2009
-	* @link http://www.coldtrick.com/
-	*/
 
 	global $CONFIG;
 	
